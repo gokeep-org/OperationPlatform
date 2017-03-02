134ab08e0b22a004d5b79c6481b9a23d942415fb
xuning <xuning@eefung.com>
2017-03-02 12:19:37 +0800

    添加oauth2相关接口，未实现

--- OauthServer/src/main/java/com/op/oauth/rest/AuthorizationRest.java
+++ OauthServer/src/main/java/com/op/oauth/rest/AuthorizationRest.java
@@ -1,8 +1,10 @@
 package com.op.oauth.rest;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 /****************************************
@@ -11,11 +13,18 @@ import javax.ws.rs.core.MediaType;
  * 如有违反，必将追究其法律责任.
  * @Auther is xuning on 2017/3/1.
  ****************************************/
-@Path("/authorization")
+@Path("/authorize")
 @Produces({MediaType.APPLICATION_JSON})
+@Consumes({MediaType.APPLICATION_JSON})
 public class AuthorizationRest {
     @GET
-    public String authorization(){
+    public String authorize(
+            @QueryParam("client_id") String client_id,
+            @QueryParam("redirect_uri") String redirect_uri,
+            @QueryParam("response_type") String response_type,
+            @QueryParam("state") String state){
+
         return null;
     }
+
 }
--- OauthServer/src/main/java/com/op/oauth/rest/TokenRest.java
+++ OauthServer/src/main/java/com/op/oauth/rest/TokenRest.java
@@ -1,10 +1,36 @@
 package com.op.oauth.rest;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
 /****************************************
  * Copyright (c) xuning.
  * 尊重版权，禁止抄袭!
  * 如有违反，必将追究其法律责任.
  * @Auther is xuning on 2017/3/1.
  ****************************************/
+@Path("/token")
+@Produces({MediaType.APPLICATION_JSON})
+@Consumes({MediaType.APPLICATION_JSON})
 public class TokenRest {
+    @POST
+    public String token(
+            @QueryParam("grant_type") @DefaultValue("password") String grantType,
+            @QueryParam("redirect_uri") String redirectUri,
+            @QueryParam("code") String code,
+            @QueryParam("refresh_token") String refreshToke,
+            @HeaderParam("authorization") String authorization){
+        if (grantType.equals("authorization_code")){
+            //authorization_code mode
+        }
+
+        //password mode
+        return "ok";
+    }
 }
