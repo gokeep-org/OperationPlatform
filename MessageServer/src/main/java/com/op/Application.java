9b59eba8a91c85afc01ceb355a279be4cef7c30d
xuning <xuning@eefung.com>
2017-03-06 17:27:53 +0800

    消息服务添加验证请求，不合适，要替换

--- MessageServer/src/main/java/com/op/Application.java
+++ MessageServer/src/main/java/com/op/Application.java
@@ -9,8 +9,10 @@ package com.op; /****************************************
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("spring.xml")
 public class Application {
 
     public static void main(String[] args) {
--- MessageServer/src/main/java/com/op/action/filter/OpRequestFilter.java
+++ MessageServer/src/main/java/com/op/action/filter/OpRequestFilter.java
@@ -3,87 +3,105 @@
  */
 package com.op.action.filter;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.slf4j.MDC;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.container.ContainerRequestContext;
-import javax.ws.rs.container.ContainerRequestFilter;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.ext.Provider;
 import java.io.IOException;
 import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.util.Enumeration;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.ext.Provider;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.op.exception.OperationPlatformException;
+import com.op.util.OpUtils;
+
+import requests.Requests;
+
 /**
  * <p>request和response过滤器。<p>
  * <p>
  * 创建日期 2016年1月11日<br>
- *
  * @author zhengpengfei<br>
  * @version $Revision$ $Date$
  * @since 1.0.0
  */
 @Provider
 public class OpRequestFilter implements ContainerRequestFilter {
-	private static final Logger LOGGER = LoggerFactory.getLogger(OpRequestFilter.class);
-	@Context
-	private HttpServletRequest request;
+    private static final Logger LOGGER = LoggerFactory.getLogger(OpRequestFilter.class);
+    @Context
+    private HttpServletRequest request;
+    @Autowired
+    private Requests requests;
 
-	public void filter(ContainerRequestContext requestContext) throws IOException {
-		getBrowerHeader(requestContext);
-		String method = requestContext.getMethod();
-		String path = requestContext.getUriInfo().getPath();
-		String requestPath = method + ":" + path;
-		MDC.put("user_id", requestContext.getHeaderString("user_id"));
-		LOGGER.info(requestContext.getHeaderString("token"));
-		LOGGER.info("------>>>请求路径：" + requestPath + " header " + requestContext.getHeaders());
-	}
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+        getBrowerHeader(requestContext);
+        String method = requestContext.getMethod();
+        String path = requestContext.getUriInfo().getPath();
+        String requestPath = method + ":" + path;
+        String accessToken = requestContext.getHeaderString("token");
+        String userId = requestContext.getHeaderString("user_id");
+        if (OpUtils.checkStringIsNull(userId) || OpUtils.checkStringIsNull(accessToken)) {
+            throw new OperationPlatformException("header date user_id or token is null");
+        }
+        if (!OpUtils.checkUserToken(userId, accessToken)){
+            throw new OperationPlatformException("Token check failed");
+        }
+        MDC.put("user_id", requestContext.getHeaderString(userId));
+        MDC.put("access_token", requestContext.getHeaderString(accessToken));
+        LOGGER.info("请求拦截到token is: "+accessToken);
+        LOGGER.info("请求拦截到user id is: "+accessToken);
+        LOGGER.info("------>>>请求路径：" + requestPath + " header " + requestContext.getHeaders());
+    }
 
-	private void getBrowerHeader(ContainerRequestContext requestContext) {
-		try {
-			String userAgent = request.getHeader("User-Agent");
-			if (userAgent != null) {
-				MDC.put("User-Agent", userAgent);
-			}
-			String userIp = request.getHeader("user_ip");
-			if (userIp != null) {
-				MDC.put("user_ip", userIp);
-			} else {
-				MDC.put("user_ip", getLocalAddress());
-			}
-		} catch (Exception e) {
-		}
-	}
+    private void getBrowerHeader(ContainerRequestContext requestContext) {
+        try {
+            String userAgent = request.getHeader("User-Agent");
+            if (userAgent != null) {
+                MDC.put("User-Agent", userAgent);
+            }
+            String userIp = request.getHeader("user_ip");
+            if (userIp != null) {
+                MDC.put("user_ip", userIp);
+            } else {
+                MDC.put("user_ip", getLocalAddress());
+            }
+        } catch (Exception e) {
+        }
+    }
 
-	public static String getLocalAddress() {
-		String ipStr = "127.0.0.1";
-		try {
-			Enumeration allNetInterfaces = NetworkInterface.getNetworkInterfaces();
-			InetAddress ip = null;
-			while (allNetInterfaces.hasMoreElements()) {
-				NetworkInterface netInterface = (NetworkInterface) allNetInterfaces.nextElement();
-				Enumeration addresses = netInterface.getInetAddresses();
-				while (addresses.hasMoreElements()) {
-					ip = (InetAddress) addresses.nextElement();
-					if (ip != null && ip instanceof Inet4Address)
-						ipStr = ip.getHostAddress();
-				}
-			}
-		} catch (Exception e) {
-			LOGGER.error("------>>>获取本机ip失败", e);
-		}
-		return ipStr;
-	}
+    public static String getLocalAddress() {
+        String ipStr = "127.0.0.1";
+        try {
+            Enumeration allNetInterfaces = NetworkInterface.getNetworkInterfaces();
+            InetAddress ip = null;
+            while (allNetInterfaces.hasMoreElements()) {
+                NetworkInterface netInterface = (NetworkInterface) allNetInterfaces.nextElement();
+                Enumeration addresses = netInterface.getInetAddresses();
+                while (addresses.hasMoreElements()) {
+                    ip = (InetAddress) addresses.nextElement();
+                    if (ip != null && ip instanceof Inet4Address)
+                        ipStr = ip.getHostAddress();
+                }
+            }
+        } catch (Exception e) {
+            LOGGER.error("------>>>获取本机ip失败", e);
+        }
+        return ipStr;
+    }
 
-	public HttpServletRequest getRequest() {
-		return request;
-	}
+    public HttpServletRequest getRequest() {
+        return request;
+    }
 
-	public void setRequest(HttpServletRequest request) {
-		this.request = request;
-	}
+    public void setRequest(HttpServletRequest request) {
+        this.request = request;
+    }
 }
