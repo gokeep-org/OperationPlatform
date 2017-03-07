05549fc16d4ab9dfef908ee0092cf673e5038387
xuning <xuning@eefung.com>
2017-03-07 20:23:28 +0800

    添加文档说明书

--- MessageServer/src/main/java/com/op/MessageBootstrap.java
+++ MessageServer/src/main/java/com/op/MessageBootstrap.java
@@ -16,8 +16,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class MessageBootstrap {
 
     public static void main(String[] args) {
-       new SpringApplicationBuilder(MessageBootstrap.class)
-               .bannerMode(Banner.Mode.OFF)
-               .run(args);
+        new SpringApplicationBuilder(MessageBootstrap.class)
+                .bannerMode(Banner.Mode.OFF)
+                .run(args);
     }
 }
