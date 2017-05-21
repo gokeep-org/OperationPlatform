2a9d435978c3c752c46906d2e46fa73fd9dc05e0
xuning <xuning@eefung.com>
2017-05-22 01:46:22 +0800

    网关权限控制完成，待加入请求事件数据

---
+++ CustomerServer/src/main/java/com/op/customer/rest/ContactRest.java
@@ -0,0 +1,10 @@
+package com.op.customer.rest;
+
+/****************************************
+ * Copyright (c) xuning.
+ * 尊重版权，禁止抄袭!
+ * 如有违反，必将追究其法律责任.
+ * @Auther is xuning on 2017/5/21.
+ ****************************************/
+public class ContactRest {
+}
